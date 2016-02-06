8e3b48dc86e4f520552685112b06e09160600058
Jacob Jenkins <jacob@jacobejenkins.com>
2016-02-06 01:15:10 -0500

    Add basic routes

---
+++ routes.js
@@ -0,0 +1,30 @@
+exports.chunkVideo = function(req, res) {
+    var name = "darude_sandstorm";
+    var command = 'ffmpeg -i videos/' + name + '/' + name + '.mp4 -acodec copy -f segment -segment_time 10 -vcodec copy -reset_timestamps 1 -map 0 -an videos/' + name + '/' + name + '%d.mp4';
+    console.log(command);
+    child = exec(command, function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
+    res.status(200).send();
+};
+
+exports.downloadYouTubeVideo = function(req, res) {
+    var yt_id = req.params.youtube_url;
+    var youtube_url = 'https://www.youtube.com/watch?v=' + yt_id;
+    console.log(yt_id);
+
+    var command = 'youtube-dl --id -f "mp4" ' + youtube_url;
+    child = exec(command, function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+
+        var child2 = exec('mkdir ' + process.env.PWD + yt_id, function(error, stdout, stderr) {
+            var ch3 = exec('mv ' + process.env.PWD + yt_id + '.mp4 ' + yt_id, function(error, stdout, stderr) {
+                console.log('stdout: ' + stdout);
+            });
+        });
+    });
+};
