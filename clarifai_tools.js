a746d04a3f915324dfc75d6fe151bc1a8e283006
Jacob Jenkins <jacob@jacobejenkins.com>
2016-02-06 12:18:35 -0500

    Clarafai API KEYS WHY

--- clarifai_tools.js
+++ clarifai_tools.js
@@ -1,79 +1,54 @@
 var Clarifai = require('./lib/clarifai_node.js');
 Clarifai.initAPI('BJCpT_mnhysKgy2n0cI6mhNZNqggpJpJmsqbwvBz', 'spcZJuQduHYjDN9BjyFTT2-I6TMSzKuFneW45xHT');
 
+Clarifai.setLogHttp(true);
 
-var stdio = require('stdio');
-
-// support some command-line options
-var opts = stdio.getopt( {
-	'print-results' : { description: 'print results'},
-	'print-http' : { description: 'print HTTP requests and responses'},
-	'verbose' : { key : 'v', description: 'verbose output'}
-});
-var verbose = opts["verbose"];
-
-Clarifai.setVerbose( verbose );
-
-Clarifai.setLogHttp( true ) ;
-
-Clarifai.setThrottleHandler( function( bThrottled, waitSeconds ) {
-	console.log( bThrottled ? ["throttled. service available again in",waitSeconds,"seconds"].join(' ') : "not throttled");
+Clarifai.setThrottleHandler(function(bThrottled, waitSeconds) {
+    console.log(bThrottled ? ["throttled. service available again in", waitSeconds, "seconds"].join(' ') : "not throttled");
 });
 
-function commonResultHandler( err, res ) {
+function resultHandler(err, res) {
 
-	if( err != null ) {
-		if( typeof err["status_code"] === "string" && err["status_code"] === "TIMEOUT") {
-			console.log("TAG request timed out");
-		}
-		else if( typeof err["status_code"] === "string" && err["status_code"] === "ALL_ERROR") {
-			console.log("TAG request received ALL_ERROR. Contact Clarifai support if it continues.");
-		}
-		else if( typeof err["status_code"] === "string" && err["status_code"] === "TOKEN_FAILURE") {
-			console.log("TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");
-		}
-		else if( typeof err["status_code"] === "string" && err["status_code"] === "ERROR_THROTTLED") {
-			console.log("Clarifai host is throttling this application.");
-		}
-		else {
-			console.log("TAG request encountered an unexpected error: ");
-			console.log(err);
-		}
-	}
-	else {
-		// if some images were successfully tagged and some encountered errors,
-		// the status_code PARTIAL_ERROR is returned. In this case, we inspect the
-		// status_code entry in each element of res["results"] to evaluate the individual
-		// successes and errors. if res["status_code"] === "OK" then all images were
-		// successfully tagged.
-		if( typeof res["status_code"] === "string" &&
-			( res["status_code"] === "OK" || res["status_code"] === "PARTIAL_ERROR" )) {
+    if (err != null) {
+        if (typeof err["status_code"] === "string" && err["status_code"] === "TIMEOUT") {
+            console.log("TAG request timed out");
+        } else if (typeof err["status_code"] === "string" && err["status_code"] === "ALL_ERROR") {
+            console.log("TAG request received ALL_ERROR. Contact Clarifai support if it continues.");
+        } else if (typeof err["status_code"] === "string" && err["status_code"] === "TOKEN_FAILURE") {
+            console.log("TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");
+        } else if (typeof err["status_code"] === "string" && err["status_code"] === "ERROR_THROTTLED") {
+            console.log("Clarifai host is throttling this application.");
+        } else {
+            console.log("TAG request encountered an unexpected error: ");
+            console.log(err);
+        }
+    } else {
+        if (typeof res["status_code"] === "string" &&
+            (res["status_code"] === "OK" || res["status_code"] === "PARTIAL_ERROR")) {
 
-			// the request completed successfully
-			for( i = 0; i < res.results.length; i++ ) {
-				if( res["results"][i]["status_code"] === "OK" ) {
-					console.log( 'docid='+res.results[i].docid +
-						' local_id='+res.results[i].local_id +
-						' tags='+res["results"][i].result["tag"]["classes"] )
-				}
-				else {
-					console.log( 'docid='+res.results[i].docid +
-						' local_id='+res.results[i].local_id +
-						' status_code='+res.results[i].status_code +
-						' error = '+res.results[i]["result"]["error"] )
-				}
-			}
-
-		}
-	}
+            // // the request completed successfully
+            // for (i = 0; i < res.results.length; i++) {
+            //     if (res["results"][i]["status_code"] === "OK") {
+            //         console.log('docid=' + res.results[i].docid +
+            //             ' local_id=' + res.results[i].local_id +
+            //             ' tags=' + res["results"][i].result["tag"]["classes"]);
+            //     } else {
+            //         console.log('docid=' + res.results[i].docid +
+            //             ' local_id=' + res.results[i].local_id +
+            //             ' status_code=' + res.results[i].status_code +
+            //             ' error = ' + res.results[i]["result"]["error"]);
+            //     }
+            // }
+            console.log(res.results);
+        }
+    }
 }
 
 
-exports.tagVideo = function(yt_url, chunk) {
-	var testImageURL = `http://subwoofer.mangohacks.com/${yt_url}/${yt_url}${chunk}.mp4`;
-	var ourId = yt_url; // this is any string that identifies the image to your system
+exports.tagVideo = function(yt_id, chunk) {
+    var testImageURL = `http://subwoofer.mangohacks.com/${yt_id}/${yt_id}${chunk}.mp4`;
 
-	Clarifai.tagURL( testImageURL , ourId, resultHandler );
-}
+    Clarifai.tagURL(testImageURL, yt_id, resultHandler);
+};
 
 Clarifai.clearThrottleHandler();
