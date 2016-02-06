94ad432beb4bdfc12c8c2fc34fa49bbe79d2ccfc
Jacob Jenkins <jacob@jacobejenkins.com>
2016-02-06 03:22:43 -0500

    Refactoring

--- routes.js
+++ routes.js
@@ -1,18 +1,15 @@
 var q = require('q');
 var exec = q.nfbind(require('child_process').exec);
 
-exports.chunkVideo = function(req, res) {
-    var name = "darude_sandstorm";
-    var command = 'ffmpeg -i videos/' + name + '/' + name + '.mp4 -acodec copy -f segment -segment_time 10 -vcodec copy -reset_timestamps 1 -map 0 -an videos/' + name + '/' + name + '%d.mp4';
+function chunkVideo(name) {
+    var command =
+            `ffmpeg -i videos/${name}/${name}.mp4 -acodec copy -f segment -segment_time 10 -vcodec copy -reset_timestamps 1 -map 0 -an videos/${name}/${name}%d.mp4`;
     console.log(command);
-    var child = exec(command, function(error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
+    exec(command).then(function(streams) {
+        console.log('ffmpeg_out:', streams[0]);
+    }).catch(function(err) {
+        console.log(err);
     });
-    res.status(200).send();
 };
 
 exports.downloadYouTubeVideo = function(req, res) {
@@ -25,19 +22,30 @@ exports.downloadYouTubeVideo = function(req, res) {
         console.log(streams[0]);
         // Make the directory
         var mkdir = 'mkdir ' + process.env.PWD + '/' + yt_id;
+
         return exec(mkdir);
+
     }).then(function(streams) {
         console.log(streams[0]);
         // Move the video
         var mv = 'mv ' + process.env.PWD + yt_id + '.mp4 ' + yt_id;
+
         return exec(mv);
+
     }).then(function(streams) {
         console.log(streams[0]);
+
+        // Chunk that video!
+        chunkVideo(yt_id);
+
         // All good buddy!
         res.status(200).send();
+
     }).catch(function(err) {
         console.log(err);
+
         // Bad stuff, give us an error
         res.status(401).send();
+
     });
 };
