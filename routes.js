58f9a0d9c3152682ab081b669514dc86db92a42d
Jacob Jenkins <jacob@jacobejenkins.com>
2016-02-06 01:18:18 -0500

    Fix broken child stuff, In the arms of an angel

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
+var exec = require('child_process').exec;
+
 exports.chunkVideo = function(req, res) {
     var name = "darude_sandstorm";
     var command = 'ffmpeg -i videos/' + name + '/' + name + '.mp4 -acodec copy -f segment -segment_time 10 -vcodec copy -reset_timestamps 1 -map 0 -an videos/' + name + '/' + name + '%d.mp4';
     console.log(command);
-    child = exec(command, function(error, stdout, stderr) {
+    var child = exec(command, function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
@@ -18,7 +20,7 @@ exports.downloadYouTubeVideo = function(req, res) {
     console.log(yt_id);
 
     var command = 'youtube-dl --id -f "mp4" ' + youtube_url;
-    child = exec(command, function(error, stdout, stderr) {
+    var child = exec(command, function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
 
         var child2 = exec('mkdir ' + process.env.PWD + yt_id, function(error, stdout, stderr) {
